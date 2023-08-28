a11494a017a698adcebafcddb67e0877210ae643
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-08-28 16:00:36 +0700

    Test

--- wsEvents/patchApp.js
+++ wsEvents/patchApp.js
@@ -73,11 +73,11 @@ async function reinstallReVanced() {
   switch (global.jarNames.selectedApp.packageName) {
     case 'com.google.android.youtube':
       if (!global.jarNames.isRooted)
-        pkgNameToGetUninstalled = 'app.revanced.android.youtube';
+        pkgNameToGetUninstalled = 'app.rvx.android.youtube';
       break;
     case 'com.google.android.apps.youtube.music':
       if (!global.jarNames.isRooted)
-        pkgNameToGetUninstalled = 'app.revanced.android.apps.youtube.music';
+        pkgNameToGetUninstalled = 'app.rvx.android.apps.youtube.music';
       break;
   }
 
@@ -156,16 +156,14 @@ module.exports = async function patchApp(ws) {
   const args = [
     '-jar',
     global.jarNames.cli,
-    'patch -b',
+    'patch --patch-bundle',
     global.jarNames.patchesJar,
-    '-m',
+    '--merge',
     global.jarNames.integrations,
-    '-t',
-    './revanced-cache',
     '--experimental',
-    '-o',
+    '--out',
     join(global.revancedDir, 'revanced.apk'),
-    '-a',
+    '--apk',
     `${join(global.revancedDir, global.jarNames.selectedApp.packageName)}.apk`
   ];
 
